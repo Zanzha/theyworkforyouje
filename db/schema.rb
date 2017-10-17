696e42e84f93a2f13ab58166014028a7eb793116
student <student@djlcp-debian9>
2017-10-17 19:42:15 +0100

    generated roleole mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017183230) do
+ActiveRecord::Schema.define(version: 20171017184127) do
 
   create_table "notices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 20171017183230) do
     t.datetime "proposition_date"
     t.datetime "vote_date"
     t.string "reference"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
