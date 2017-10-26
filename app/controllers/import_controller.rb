ff29e820dbbc041aaa0a752f91f82281858d29a0
Al Craig <contact@alcraig.com>
2017-10-26 18:53:41 +0100

    Updates to JSON pull loop

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -2,14 +2,17 @@ class ImportController < ApplicationController
   require 'open-uri'
   def index
     Vote.delete_all
-    data_uri = 'http://www.statesassembly.gov.je/Feeds/VotingDataJSON?Year=2014'
-    raw_data = open(data_uri).read
-    @data = JSON.parse(raw_data)
-    @data.each do |item|
-      vote = Vote.new
-      vote.proposition_id = item['Id']
-      vote.type = item['Vote']
-      vote.save
+    array = ['2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016','2017']
+    array.each do |year|
+      data_uri = "http://www.statesassembly.gov.je/Feeds/VotingDataJSON?Year=#{year}"
+      raw_data = open(data_uri).read
+      @data = JSON.parse(raw_data)
+      @data.each do |item|
+        vote = Vote.new
+        vote.proposition_id = item['Id']
+        vote.type = item['Vote']
+        vote.save
+      end
     end
     puts 'Data import complete'
   end
