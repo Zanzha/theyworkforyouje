94b8bdc146aaac1b0c00eaf1e56d549b5da2cad1
Taylor Le Fustec <taylorelliott96@icloud.com>
2017-10-26 19:31:02 +0100

    tweaking stuff

--- db/schema.rb
+++ db/schema.rb
@@ -71,31 +71,14 @@ ActiveRecord::Schema.define(version: 20171026174310) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "scraped_props", id: :integer, force: :cascade, options: "ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin" do |t|
-    t.string "p_id", null: false, collation: "latin1_swedish_ci"
-    t.string "lodged_by", null: false
-    t.string "prop_name", null: false
-    t.string "prop_date", null: false, collation: "latin1_swedish_ci"
-    t.string "debate_date", null: false, collation: "latin1_swedish_ci"
-    t.string "prop_pdf", null: false, collation: "latin1_swedish_ci"
-    t.string "minutes_pdf", null: false, collation: "latin1_swedish_ci"
-    t.string "hansard_pdf", null: false, collation: "latin1_swedish_ci"
-    t.string "vote_id", null: false, collation: "latin1_swedish_ci"
-    t.string "status", null: false, collation: "latin1_swedish_ci"
-    t.index ["p_id"], name: "p_id", unique: true
-  end
-
   create_table "terms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "parish_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "politician_id"
+    t.integer "office_id"
     t.datetime "begin_date"
     t.datetime "end_date"
-    t.bigint "parishes_id"
-    t.bigint "politician_id"
-    t.bigint "offices_id"
-    t.index ["offices_id"], name: "index_terms_on_offices_id"
-    t.index ["parishes_id"], name: "index_terms_on_parishes_id"
-    t.index ["politician_id"], name: "index_terms_on_politician_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -118,13 +101,12 @@ ActiveRecord::Schema.define(version: 20171026174310) do
   end
 
   create_table "votes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "proposition_id"
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "propositions_id"
     t.string "member_position"
     t.string "member_name"
-    t.index ["propositions_id"], name: "index_votes_on_propositions_id"
   end
 
   add_foreign_key "notices", "users"
