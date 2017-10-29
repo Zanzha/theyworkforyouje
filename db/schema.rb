b2aeb6364a0ffe9e85f0a25ca572ecbf79f418fb
Martin <martinaraignee@hotmail.com>
2017-10-29 15:06:20 +0000

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026183742) do
+ActiveRecord::Schema.define(version: 20171026185848) do
 
   create_table "notices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -109,6 +109,9 @@ ActiveRecord::Schema.define(version: 20171026183742) do
     t.datetime "updated_at", null: false
     t.string "member_position"
     t.string "member_name"
+    t.string "voting_id"
+    t.datetime "voting_date"
+    t.string "proposition_title"
   end
 
   add_foreign_key "notices", "users"
