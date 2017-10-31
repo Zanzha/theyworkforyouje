4de5e58914cb72683603288d494eec7f33ef43b9
Rob Dudley <robert.dudley@calligo.cloud>
2017-10-31 18:59:30 +0000

    Right order

--- db/migrate/20171020182230_add_foreign_key_to_politician.rb
+++ db/migrate/20171020182230_add_foreign_key_to_politician.rb
@@ -1,8 +1,8 @@
 class AddForeignKeyToPolitician < ActiveRecord::Migration[5.1]
   def change
 
-    change_column :politician, :parish_id, :bigint
-    add_foreign_key :politician, :parishes
+    change_column :politicians, :parish_id, :bigint
+    add_foreign_key :politicians, :parishes
 
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+Role.create!([
+  {name: "Admin"},
+  {name: "Guest"}
+])
 User.create!([
-  {email: "admin@admin.com", encrypted_password: "$2a$11$t8CeUAmwbxQVMPX.k8HHFuOeCWxEeqOregdH4OGu1o/WXTzaoG22i", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, role_id: 1, created_by: "devteam", username: "Captain Placeholder"}
+  {email: "admin@admin.com", password: "topsecret", password_confirmation: "topsecret", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, role_id: 1, created_by: "devteam", username: "Captain Placeholder"}
 ])
 Parish.create!([
   {name: "Saint Helier"},
@@ -15,7 +19,4 @@ Parish.create!([
   {name: "Saint Saviour"},
   {name: "Trinity"}
 ])
-Role.create!([
-  {name: "Admin"},
-  {name: "Guest"}
-])
+
