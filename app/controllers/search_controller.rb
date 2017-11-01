6f8b779ba0a04e23ef397de4fefcaf02a1d8284e
Rob Dudley <robert.dudley@calligo.cloud>
2017-11-01 12:04:17 +0000

    Working search with model controller and view changes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,34 +1,39 @@
 class SearchController < ApplicationController
   def index
-    
-    if !params['q']
+
+    #  Check if we have the two required parameters to run a search
+    # if not we can simply return the view
+    if (!params['q'] || ! params['searchOn'])
       return
     end
 
-    query = params['q']
-    searchOn = params[searchOn]
+    # Set up an array to hold our results (if any)
+    @results = Array.new
 
-
-    case searchOn
-      when 'politician'
-        results = Politician.searchBy(query)
-        results.each.do |result|
-          cleanresult = {
-            'title': result.full_name,
-            'link': link_to(Politician(result.id))
-          }
-          @results[] = cleanResult
-        end
-          
-      when 'proposition'
-        results = Proposition.searchBy(query)
-        results.each.do |result|
-          @results[]['title'] = result.prop_name
-        end
-      else
-        # Nope
-        @results = []
+    # Run the search
+    if params['searchOn'] == 'politician'      
+      searchResults = Politician.search(params['q']) # Note search is a model method
+    elsif searchOn == 'proposition'
+      searchResults = Proposition.search(params['q']) # Note search is a model method
     end
+
+    # Loop over the results and make them consistent
+    searchResults.each do |searchResult|
+      if searchOn == 'politician'
+        result_label = searchResult.full_name
+        result_link = view_context.link_to("View more", politician_path(searchResult))
+      elsif searchOn == 'proposition'
+        result_label = searchResult.prop_name
+        result_link = view_context.link_to("View more", proposition_path(searchResult))
+      end
+
+      # Add this result ot the Array of results
+      @results.push( {
+        'label' => result_label, 
+        'link' => result_link
+      })
+    end
+
   end
 
 end
