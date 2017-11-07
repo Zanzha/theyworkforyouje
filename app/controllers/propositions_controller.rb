2230d041c93be219ea6881488205f1b59cd7329f
Zanzha <WOooblox@gmail.com>
2017-11-07 19:58:29 +0000

    i did something

--- app/controllers/propositions_controller.rb
+++ app/controllers/propositions_controller.rb
@@ -1,5 +1,6 @@
 class PropositionsController < ApplicationController
   before_action :set_proposition, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /propositions
   # GET /propositions.json
