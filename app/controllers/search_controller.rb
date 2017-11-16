a3620c125a4c51e59dc325e1a7499dcc16ad9ee5
Martin <martinaraignee@hotmail.com>
2017-11-16 13:55:57 +0000

    initial commit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -22,8 +22,8 @@ class SearchController < ApplicationController
 
       # Add this result ot the Array of results
       @pol_results.push({
-        'label' => pol_result_label, 
-        'title' => pol_result_title, 
+        'label' => pol_result_label,
+        'title' => pol_result_title,
         'link' => pol_result_link
       })
     end
@@ -31,20 +31,20 @@ class SearchController < ApplicationController
 
       # Set up an array to hold our PROPOSITION results (if any)
       @prop_results = Array.new
-      
+
       # Run the search & Limits search results.
         propResults = Proposition.search(params['q']).page params[:page] # Note search is a model method
-  
+
       # Loop over the results and make them consistent
       propResults.each do |propResult|
         prop_result_label = propResult.prop_name
         prop_result_proposer = propResult.lodged_by
         prop_result_link = view_context.link_to("View more", proposition_path(propResult))
-  
+
         # Add this result ot the Array of results
         @prop_results.push({
-          'label' => prop_result_label, 
-          'proposer' => prop_result_proposer, 
+          'label' => prop_result_label,
+          'proposer' => prop_result_proposer,
           'link' => prop_result_link
         })
       end
