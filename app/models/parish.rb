7cf586f3606cc4880f6e48df229b6eb39165aab8
Al <contact@alcraig.com>
2017-11-23 18:54:32 +0000

    Correct text with deletion

--- app/models/parish.rb
+++ app/models/parish.rb
@@ -70,7 +70,7 @@ class Parish < ApplicationRecord
         Population 3,156 (2011)
         Density	260/km2 (660/sq mi).'
       elsif self.name == 'Island Wide'
-        'Senators represent the whole Island. All 8 Senators are elected for the same term of office as the Constables and Deputies.  Deputies are elected in 17 districts which correspond to parish boundaries. Larger parishes are divided u​p into more than 1 district to make sure the population is represented (e.g. St Helier and St Brelade).'
+        'Senators represent the whole Island. All 8 Senators are elected for the same term of office as the Constables and Deputies.'
       else
         'Parish'
       end
